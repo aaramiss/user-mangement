e941a129051e2ad8a314bbabd6d333ba1c65b8ba
SamiraMiss <samirarostami71@gmail.com>
2023-10-05 16:23:57 +0330

    define state, action, reducer for start of redux toolkit.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./app/store";
-import "./index.css";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <StrictMode>
